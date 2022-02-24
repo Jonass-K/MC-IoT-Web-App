0bfaecdb46a97442c98d77b4b347dc08a7a8ca5a
Jonas <Joonaskaiser@gmail.com>
2022-02-24 17:52:05 +0100

    Fix github workflow build and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "./index.html",
+                    to: `${__dirname}/dist/index.html`,
+                },
+                {
+                    from: "./app/style/index.css",
+                    to: `${__dirname}/dist/app/style/index.css`,
+                },
+            ],
+        }),
+    ],
 
     module: {
         rules: [
@@ -19,7 +34,7 @@ module.exports = {
 
     entry: "./app/src/index.ts",
     output: {
-        path: path.resolve(__dirname, "app/dist"),
+        path: path.resolve(__dirname, "dist/dist"),
         filename: "bundle.js"
     },
 }
